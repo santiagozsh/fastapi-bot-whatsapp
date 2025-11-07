e4246d0c82323832a4a7d466cc5243604c43deee
santiago <sanrodsuz@pm.me>
2025-11-07 09:51:06 -0500

    #1 main.py: creating connection towards google-sheets

--- main.py
+++ main.py
@@ -1,11 +1,36 @@
-import os
-import re
-import gspread
-# import uvicorn
-from fastapi import FastAPI, Request, HTTPException, Response
-from dotenv import load_dotenv
+import os;
+import re;
+import gspread;
+from fastapi import FastAPI, Request, HTTPException, Response;
+from dotenv import load_dotenv;
 
-app = FastAPI()
+app = FastAPI();
+
+print("Loading .env vars");
+load_dotenv();
+
+whatsapp_token = os.getenv("WHATSAPP_VERIFY_TOKEN");
+google_sheet_name = os.getenv("GOOGLE_SHEET_NAME");
+trigger_word = os.getenv("TRIGGER_WORD", "!pedido");
+
+# print(f"wt: { whatsapp_token }, gsn: { google_sheet_name }, tw: { trigger_word }")
+
+print(f"{ google_sheet_name }");
+
+# google sheets connection
+try:
+    # auth with google using service_account.json file
+    key_auth = gspread.service_account(filename="service_account.json");
+    # open the spreadsheet with the specific name 
+    spreadsheet = key_auth.open(google_sheet_name);
+
+    sales = spreadsheet.worksheet("Ventas");
+    transaction = spreadsheet.worksheet("Ingresos transacciones");
+    print(f"INFO: google sheet connection: {google_sheet_name} success!");
+
+except Exception as error:
+    print("ERROR: Google connection is not working. Check service_account.json or permissions!");
+    print(f"Trace Exception: { error }");
 
 
 @app.get("/")
