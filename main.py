d6dbfa7147853b13d073a6b675d625d5e3d980fc
santiago <sanrodsuz@pm.me>
2025-11-07 12:00:23 -0500

    #2 main.py: adding new endpoint for adding data into the sheet

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ try:
     spreadsheet = key_auth.open(google_sheet_name);
 
     sales = spreadsheet.worksheet("Ventas");
-    transaction = spreadsheet.worksheet("Ingresos transacciones");
+    # transaction = spreadsheet.worksheet("Ingresos transacciones");
     print(f"INFO: google sheet connection: {google_sheet_name} success!");
 
 except Exception as error:
@@ -37,3 +37,14 @@ except Exception as error:
 async def root():
     return {"hola": "mundo"}
 
+@app.get("/append-data")
+def test ():
+    try:
+        data_row = ["LG-03", "1-Sep-2025", "Wilmer alexander ayas", "312 547 8594", "Mosquera", "CUNDINAMARCA", "1 kairo", 3];
+        sales.append_row(data_row, value_input_option="RAW");
+        print(f"INFO: datos agregados { data_row }");
+        return {"status": "prueba existosa!"};
+
+    except Exception as error:
+        print(f"ERROR:    Error al escribir en Google Sheets: { error }")
+        raise HTTPException(status_code=500, detail=f"Error al escribir en Sheets: { error }")
