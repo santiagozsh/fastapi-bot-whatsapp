aa9cb4c30aae3c767247b22076a4eae4684714f4
santiago <sanrodsuz@pm.me>
2025-11-20 11:31:39 -0500

    UPDATE: adding new variables in EnviromentSettings class

--- core/config.py
+++ core/config.py
@@ -4,11 +4,14 @@ class EnviormentSettings(BaseSettings):
     WHATSAPP_VERIFY_TOKEN: str
     GOOGLE_SHEET_NAME: str
     TRIGGER_WORD: str = "!pedido"
+    WHATSAPP_API_TOKEN: str
+    WHATSAPP_PHONE_NUMBER_ID: str
+    WHATSAPP_BUSINESS_ACCOUNT_ID: str 
 
     class Config:
         env_file = ".env"
 
 env_settings = EnviormentSettings() # type: ignore 
 
-print(f"INFO: config load is: { env_settings.WHATSAPP_VERIFY_TOKEN } { env_settings.GOOGLE_SHEET_NAME }")
+print(f"INFO: config load is: { env_settings.WHATSAPP_VERIFY_TOKEN } { env_settings.GOOGLE_SHEET_NAME } { env_settings.WHATSAPP_API_TOKEN }")
 
